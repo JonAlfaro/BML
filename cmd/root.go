6c091fbd6701a86e2d40b712b74dc4c6e9a2b4b0
JonAlfaro <j_alfa_4@outlook.com>
2020-05-01 18:23:53 -0700

    Check Added to make sure that BML is installed before bookmark logic can be executed

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,10 @@ package cmd
 import (
   "fmt"
   "github.com/spf13/cobra"
+  "log"
   "os"
+    "os/user"
+    "path/filepath"
 )
 
 
@@ -14,9 +17,32 @@ var rootCmd = &cobra.Command{
   Use:   "BML",
   Short: "BML is an application that manages your terminals working directories",
   Long: `BML is an application that manages your terminals working directories`,
-  // Uncomment the following line if your bare application
   // has an action associated with it:
-  //	Run: func(cmd *cobra.Command, args []string) { },
+  	Run: func(cmd *cobra.Command, args []string) {
+        // Get the directory where the binary was called from
+  	    currentBinLocation, err := filepath.Abs(filepath.Dir(os.Args[0]))
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        // Get the directory of where the binary should be installed for this user
+        usr, err := user.Current()
+        if err != nil {
+            log.Fatal( err )
+        }
+        installationBinLocation := fmt.Sprintf("%s/.BML_Installation", usr.HomeDir)
+
+        // Check if binary is in the correct location
+        if currentBinLocation != installationBinLocation {
+            fmt.Printf(`BML binary cannot be called as it is not in the correct install location
+BML binary curently at:  %s
+BML binary should be at: %s
+Have you executed "BML install" command?`+"\n", currentBinLocation, installationBinLocation)
+            os.Exit(1)
+        }
+
+        // Main Functionality of BML
+    },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
